3012bf6f2edb9a321f8aed3070bb642b096bd1da
Samson Njai <njaisamson@gmail.com>
2024-07-02 17:54:44 +0300

    manipulated the cats array as required

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  return  cats.push(name = "Ralph");
+}
+
+function destructivelyPrependCat(name) {
+   return cats.unshift(name = "Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    return [...cats, "Broom"];
+}
+
+function prependCat(name) {
+    return ["Arnold", ...cats];
+}
+
+function removeLastCat() {
+   return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
